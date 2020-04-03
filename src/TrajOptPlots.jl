75fba8f6b20988b5b76505b3233e05c520fe7886
Brian Jackson <bjack205@gmail.com>
2020-04-03 09:44:22 -0700

    Update deps

--- src/TrajOptPlots.jl
+++ src/TrajOptPlots.jl
@@ -5,18 +5,24 @@ using GeometryTypes
 using Rotations
 using CoordinateTransformations
 using TrajectoryOptimization
+using TrajOptCore
 using Plots
 using StaticArrays
 using FileIO
 using MeshIO
+using RobotDynamics
+using RobotZoo
 
-import TrajectoryOptimization.Dynamics: orientation
+import RobotDynamics: orientation
 
 export
     visualize!,
     plot
 
 include("rigid_bodies.jl")
+include("line_plots.jl")
+include("2d_vis.jl")
+include("models.jl")
 
 function Plots.plot(A::Vector{<:SVector{N}}, inds=1:N; kwargs...) where N
     n = length(inds)
