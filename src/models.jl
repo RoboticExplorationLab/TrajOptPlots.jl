f8eadba69a88b1b7bf0a64f677a4b256900984e6
Brian Jackson <bjack205@gmail.com>
2020-09-23 10:27:33 -0400

    Add quad scaling option

--- src/models.jl
+++ src/models.jl
@@ -81,10 +81,10 @@ function _set_mesh!(vis, model::RobotZoo.DubinsCar)
 end
 
 # Quadrotor
-function _set_mesh!(vis, model::RobotZoo.Quadrotor)
+function _set_mesh!(vis, model::RobotZoo.Quadrotor; scaling=1.0)
     urdf_folder = joinpath(@__DIR__, "..", "data", "meshes")
     obj = joinpath(urdf_folder, "quadrotor_base.obj")
-    quad_scaling = 0.085
+    quad_scaling = 0.085 * scaling
     # quad_scaling = 0.15
     robot_obj = FileIO.load(obj)
     robot_obj.vertices .= robot_obj.vertices .* quad_scaling
